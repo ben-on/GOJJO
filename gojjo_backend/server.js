bd91fe3e7ed41c168fde1c79dea332d88170e73a
ben-on <biniyamalemu595@gmail.com>
2024-03-25 21:33:04 +0300

    add get post

--- gojjo_backend/server.js
+++ gojjo_backend/server.js
@@ -3,6 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config()
 
+// dummy post
+const posts = [
+    {
+        user: "benon",
+        content: "This is benon"
+    }
+]
+
 const gojjo = express();
 
 gojjo.get('/', (req, res) => {
@@ -11,3 +19,7 @@ gojjo.get('/', (req, res) => {
 
 gojjo.listen(process.env.PORT,() => {console.log(`Gojjo is running at port ${process.env.PORT}`)})
 
+
+gojjo.get('/post', (req, res) => {
+    res.status(200).json(posts)
+})
